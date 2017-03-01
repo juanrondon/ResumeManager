f2d28c56186ce901566c26f5d3134ec0bde9ba44
Juan Rondon <juanrondonc@outlook.com>
2017-03-01 14:42:17 +1100

    Updated UI

--- src/ResumeManager.UI/wwwroot/_references.js
+++ src/ResumeManager.UI/wwwroot/_references.js
@@ -1,5 +1,5 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="js/site.js" />
+/// <reference path="js/createresume.min.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/handlebars/handlebars.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
---
+++ src/ResumeManager.UI/wwwroot/js/createResume.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    $(document).ready(function () {
+        $.validator.setDefaults({ ignore: "" });
+
+        $("#references").wysiwyg({
+            
+        });
+
+        var $imageupload = $(".imageupload");
+        $imageupload.imageupload({
+            allowedFormats: ["jpg", "png"],
+            maxWidth: 150,
+            maxHeight: 150,
+            maxFileSizeKb: 512
+        });
+    });
+})();
